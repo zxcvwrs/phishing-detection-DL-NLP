e7a4205432903c229d365e8ed3c94401e3635851
zxcvwrs <wolertr@gmail.com>
2023-05-07 23:27:05 +0200

    Working oAuth2 token handling and saving

--- browser_extension/scripts/oauth.js
+++ browser_extension/scripts/oauth.js
@@ -1,24 +1,64 @@
 window.onload = function() {
-    document.querySelector('button').addEventListener('click', function() {
-      chrome.identity.getAuthToken({interactive: true}, function(token) {
-        if (token) {
-            // window.location.href = 'second_view.html'
-            storeAuthToken(token);
-            getAuthToken();
+  checkAuthToken()
+      .then(token => {
+          console.log('Token', token)
+          if (token) {
+              redirectToDetectionPage();
+              console.log('Token is already saved. This would be redirection.');
+          } else {
+            getAndSetAuthToken();
+          }
+      })
+      .catch(error => {
+          console.error("Error:", error);
+      });
+};
+
+function redirectToDetectionPage() {
+  window.location.href = "detection.html";
+}
+
+function checkAuthToken() {
+  return new Promise((resolve, reject) => {
+      chrome.storage.local.get(["AuthTokenKey"], (result) => {
+        if (result) {
+          resolve(result.AuthTokenKey);
         }
-        console.log(token);
+        else {
+          reject(new Error('checkAuthToken error.'));
+        }
       });
-    });
-  };
-
-function storeAuthToken(AuthToken) {
-    chrome.storage.session.set({ AuthTokenKey: AuthToken }).then(() => {
-        console.log("Value is set to " + AuthToken);
-      });
+  });
 }
 
-function getAuthToken() {
-    chrome.storage.session.get(["AuthTokenKey"]).then((result) => {
-        return result.key
-      });
+function getAndSetAuthToken() {
+  chrome.identity.getAuthToken({interactive: true}, (token) => {
+      if (token) {
+          setAuthToken(token)
+              .then(() => {
+                  redirectToDetectionPage();
+                  console.log('Token stored', token);
+              })
+              .catch(error => {
+                  console.error("Error:", error);
+              });
+      } else {
+          console.log("getAndSetAuthToken returned empty token.");
+      }
+  });
 }
+
+function setAuthToken(AuthToken) {
+  return new Promise((resolve, reject) => {
+      chrome.storage.local.set({AuthTokenKey: AuthToken}, () => {
+          console.log("Value is set to" + AuthToken);
+          if (AuthToken) {
+            resolve();
+          }
+          else {
+            reject(new Error("storeAuthToken error."))
+          }
+      });
+  });
+}
+
